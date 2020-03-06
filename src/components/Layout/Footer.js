02d1c7383adfdd7a4dd9c00fbc3b55a53efe20c9
Shugmi Shumunov <SShumunov@gmail.com>
2020-03-06 13:34:23 -0500

    Update Footer

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -4,7 +4,7 @@ class Footer extends Component {
 	render() {
 		return(
 			<div className="footer">
-				<p>Built by Shugmi Shumunov &copy; 2019. All Rights Reserved</p>
+				<p>Built by Shugmi Shumunov &copy; 2020. All Rights Reserved</p>
 			</div>
 		)
 	}
