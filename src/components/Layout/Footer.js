8a7a2bbe0ae439946964c2ef0758ca8c9992d5b9
Shugmi Shumunov <SShumunov@gmail.com>
2021-01-21 15:20:08 -0500

    Update copyright date

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -4,7 +4,7 @@ export default class Footer extends Component {
 	render() {
 		return(
 			<div className="footer">
-				<p>Built by Shugmi Shumunov &copy; 2020. All Rights Reserved</p>
+				<p>Built by Shugmi Shumunov &copy; 2021. All Rights Reserved</p>
 			</div>
 		)
 	}
