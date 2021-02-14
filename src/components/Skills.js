64411d30c8f36da092e65bc9c0f793f036800df5
Shugmi Shumunov <SShumunov@gmail.com>
2021-02-14 13:14:58 -0500

    Update skills component if progress prop is null

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -7,8 +7,11 @@ export default class Skills extends Component {
 			<Grid>
 				<Cell col={ 12 }>
 					<div className="progress-container">
-						<span className="bold-text">{ this.props.skill }</span>
-						<ProgressBar className="skill-progress" progress={ this.props.progress }/>
+						<span className={`bold-text ${ this.props.progress === null && 'skill-name-centered'}`}>{ this.props.skill }</span>
+						{
+							this.props.progress !== null &&
+							<ProgressBar className="skill-progress" progress={ this.props.progress }/>
+						}
 					</div>
 				</Cell>
 			</Grid>
