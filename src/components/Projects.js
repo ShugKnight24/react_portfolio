97cfbed33f04c18d9d65f2e8be50431332355d51
Shugmi Shumunov <SShumunov@gmail.com>
2021-03-08 21:59:27 -0500

    Add new projects to portfolio

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -28,6 +28,54 @@ export default class Projects extends Component {
 								</Button>
 							</CardActions>
 						</Card>
+						<Card shadow={ 5 } className="project-cards">
+							<CardTitle className="project-cards-title  mappr" role="img" aria-label="Image of JavaScript Mappr project built by Shugmi Shumunov">
+								<span className="title-background">
+									Mappr
+								</span>
+							</CardTitle>
+							<CardText className="project-description">Map project built using OpenStreet map and Leaflet.js - <span className="bold-text">Still in development</span></CardText>
+							<CardActions border>
+								<Button colored>
+									<a href="https://github.com/ShugKnight24/Mappr" rel="noopener noreferrer" target="_blank">GitHub Repository</a>
+								</Button>
+								<Button colored>
+									<a href="https://shugknight24.github.io/Mappr" rel="noopener noreferrer" target="_blank">Live Demo</a>
+								</Button>
+							</CardActions>
+						</Card>
+						<Card shadow={ 5 } className="project-cards">
+							<CardTitle className="project-cards-title  quizzlr" role="img" aria-label="Image of JavaScript Quizzlr project built by Shugmi Shumunov">
+								<span className="title-background">
+									Quizzlr
+								</span>
+							</CardTitle>
+							<CardText className="project-description">Quiz project built using jQuery. Custom modal system - <span className="bold-text">Still in development</span></CardText>
+							<CardActions border>
+								<Button colored>
+									// <a href="https://github.com/ShugKnight24/pomodoro" rel="noopener noreferrer" target="_blank">GitHub Repository</a>
+								</Button>
+								<Button colored>
+									<a href="https://shumunovsolutions.com/quiz" rel="noopener noreferrer" target="_blank">Live Demo</a>
+								</Button>
+							</CardActions>
+						</Card>
+						<Card shadow={ 5 } className="project-cards">
+							<CardTitle className="project-cards-title  dooer" role="img" aria-label="Image of JavaScript Quizzlr project built by Shugmi Shumunov">
+								<span className="title-background">
+									Dooer
+								</span>
+							</CardTitle>
+							<CardText className="project-description">To-Do App built in React JS - Features a custom kanban board, basic to-do app, and pomodoro timer - <span className="bold-text">Still in development</span></CardText>
+							<CardActions border>
+								<Button colored>
+									// <a href="https://github.com/ShugKnight24/pomodoro" rel="noopener noreferrer" target="_blank">GitHub Repository</a>
+								</Button>
+								<Button colored>
+									<a href="https://shumunovsolutions.com/dooer" rel="noopener noreferrer" target="_blank">Live Demo</a>
+								</Button>
+							</CardActions>
+						</Card>
 					</div>
 				</div>
 			)
