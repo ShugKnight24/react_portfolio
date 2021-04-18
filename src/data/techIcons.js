d7c14cee0c5ba0e3434f58161202af0be6847875
Shugmi Shumunov <SShumunov@gmail.com>
2021-04-18 02:09:44 -0400

    Update how tech icons are rendered
    - Move LandingPage to new page directory
    - Make a tech icon component
    - Generate tech icons by mapping over dataset
    - Update styles
    - Add tech icon assets to the public img directory

---
+++ src/data/techIcons.js
@@ -0,0 +1,66 @@
+export const techIcons = [
+	{
+		iconName: 'HTML',
+		iconURL: '../img/tech_icons/html.svg'
+	},
+	{
+		iconName: 'CSS',
+		iconURL: '../img/tech_icons/css.svg'
+	},
+	{
+		iconName: 'SASS',
+		iconURL: '../img/tech_icons/sass.svg'
+	},
+	{
+		iconName: 'BootStrap',
+		iconURL: '../img/tech_icons/bootstrap.svg'
+	},
+	{
+		iconName: 'JavaScript',
+		iconURL: '../img/tech_icons/javascript.svg'
+	},
+	{
+		iconName: 'TypeScript',
+		iconURL: '../img/tech_icons/typescript.svg'
+	},
+	{
+		iconName: 'jQuery',
+		iconURL: '../img/tech_icons/jquery.svg'
+	},
+	{
+		iconName: 'React',
+		iconURL: '../img/tech_icons/react.svg'
+	},
+	{
+		iconName: 'React Native',
+		iconURL: '../img/tech_icons/react.svg'
+	},
+	{
+		iconName: 'NodeJS',
+		iconURL: '../img/tech_icons/nodejs.svg'
+	},
+	{
+		iconName: 'PHP',
+		iconURL: '../img/tech_icons/php.svg'
+	},
+	{
+		iconName: 'MySQL',
+		iconURL: '../img/tech_icons/mysql.svg'
+	},
+	{
+		iconName: 'Shopify',
+		iconURL: '../img/tech_icons/shopify.svg'
+	},
+	{
+		iconName: 'Shopify Liquid',
+		iconURL: '../img/tech_icons/shopify_liquid.svg'
+	},
+	{
+		iconName: 'Rust',
+		iconURL: '../img/tech_icons/rust.svg'
+	},
+	{
+		iconName: 'Python',
+		iconURL: '../img/tech_icons/python.svg'
+	}
+];
