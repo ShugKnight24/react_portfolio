94814be406543f6a3096eb0b054534fbbdc73a88
Shugmi Shumunov <SShumunov@gmail.com>
2021-04-18 00:52:45 -0400

    Initial work adding tech icons to landing page

--- src/components/Landingpage.js
+++ src/components/Landingpage.js
@@ -1,5 +1,20 @@
 import React, { Component } from 'react';
 import { Grid, Cell } from 'react-mdl';
+import HTMLIcon from '../static/img/tech_icons/html.svg';
+import CSSIcon from '../static/img/tech_icons/css.svg';
+import SASSIcon from '../static/img/tech_icons/sass.svg';
+import BootstrapIcon from '../static/img/tech_icons/bootstrap.svg';
+import JavaScriptIcon from '../static/img/tech_icons/javascript.svg';
+import TypeScriptIcon from '../static/img/tech_icons/typescript.svg';
+import JqueryIcon from '../static/img/tech_icons/jquery.svg';
+import ReactIcon from '../static/img/tech_icons/react.svg';
+import NodeJSIcon from '../static/img/tech_icons/nodejs.svg';
+import PHPIcon from '../static/img/tech_icons/php.svg';
+import MySQLIcon from '../static/img/tech_icons/mysql.svg';
+import ShopifyIcon from '../static/img/tech_icons/shopify.svg';
+import ShopifyLiquidIcon from '../static/img/tech_icons/shopify_liquid.svg';
+import RustIcon from '../static/img/tech_icons/rust.svg';
+import PythonIcon from '../static/img/tech_icons/python.svg';
 
 export default class Landing extends Component {
 	render() {
@@ -37,61 +52,85 @@ export default class Landing extends Component {
 							<hr />
 							<div>
 								<span className="bar">|</span>
-								<p>
-									HTML
-								</p>
+								<div className="tech-skill">
+									<img src={ HTMLIcon } alt="HTML5 Icon" />
+									<p>HTML</p>
+								</div>
 								<span className="bar">|</span>
-								<p>
-									CSS
-								</p>
+								<div className="tech-skill">
+									<img src={ CSSIcon } alt="CSS3 Icon" />
+									<p>CSS</p>
+								</div>
 								<span className="bar">|</span>
-								<p>
-									SASS / LESS
-								</p>
+								<div className="tech-skill">
+									<img src={ SASSIcon } alt="SASS Icon" />
+									<p>SASS</p>
+								</div>
 								<span className="bar">|</span>
-								<p>
-									BEM
-								</p>
+								<div className="tech-skill">
+									<img src={ BootstrapIcon } alt="Bootstrap Icon" />
+									<p>BootStrap</p>
+								</div>
 								<span className="bar">|</span>
-								<p>
-									BootStrap
-								</p>
+								<div className="tech-skill">
+									<img src={ JavaScriptIcon } alt="JavaScript Icon" />
+									<p>JavaScript</p>
+								</div>
 								<span className="bar">|</span>
-								<p>
-									JavaScript
-								</p>
+								<div className="tech-skill">
+									<img src={ TypeScriptIcon } alt="TypeScript Icon" />
+									<p>TypeScript</p>
+								</div>
 								<span className="bar">|</span>
-								<p>
-									jQuery
-								</p>
+								<div className="tech-skill">
+									<img className="jquery" src={ JqueryIcon } alt="jQuery Icon" />
+									<p>jQuery</p>
+								</div>
 								<span className="bar">|</span>
-								<p>
-									React
-								</p>
+								<div className="tech-skill">
+									<img className="react" src={ ReactIcon } alt="React Icon" />
+									<p>React</p>
+								</div>
 								<span className="bar">|</span>
-								<p>
-									React Native
-								</p>
+								<div className="tech-skill">
+									<img className="react" src={ ReactIcon } alt="React Native Icon" />
+									<p>React Native</p>
+								</div>
 								<span className="bar">|</span>
-								<p>
-									Node.js
-								</p>
+								<div className="tech-skill">
+									<img className="node" src={ NodeJSIcon } alt="NodeJS Icon" />
+									<p>Node.js</p>
+								</div>
 								<span className="bar">|</span>
-								<p>
-									PHP
-								</p>
+								<div className="tech-skill">
+									<img src={ PHPIcon } alt="PHP Icon" />
+									<p>PHP</p>
+								</div>
 								<span className="bar">|</span>
-								<p>
-									MySQL
-								</p>
+								<div className="tech-skill">
+									<img className="mysql" src={ MySQLIcon } alt="MySQL Icon" />
+									<p>MySQL</p>
+								</div>
 								<span className="bar">|</span>
-								<p>
-									Shopify
-								</p>
+								<div className="tech-skill">
+									<img src={ ShopifyIcon } alt="Shopify Icon" />
+									<p>Shopify</p>
+								</div>
 								<span className="bar">|</span>
-								<p>
-									Liquid
-								</p>
+								<div className="tech-skill">
+									<img src={ ShopifyLiquidIcon }alt="Shopify Liquid Icon" />
+									<p>Liquid</p>
+								</div>
+								<span className="bar">|</span>
+								<div className="tech-skill">
+									<img src={ RustIcon } alt="Rust Icon" />
+									<p>Rust</p>
+								</div>
+								<span className="bar">|</span>
+								<div className="tech-skill">
+									<img src={ PythonIcon } alt="Python Icon" />
+									<p>Python</p>
+								</div>
 								<span className="bar">|</span>
 							</div>
 							<div className="social-links">
