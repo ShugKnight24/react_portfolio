a28480b977397c719c381331ef5f5b534a94a2ca
Shugmi Shumunov <SShumunov@gmail.com>
2021-04-21 12:33:09 -0400

    Seperate components for project section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,75 +1,16 @@
-import { Card, CardTitle, CardText, CardActions, Button } from 'react-mdl';
+import { PersonalProjects } from './Projects/PersonalProjects';
+import { JavaScript30 } from './Projects/JavaScript30';
+import { Games } from './Projects/Games';
+import { GTB } from './Projects/GTB';
+import { LoveBook } from './Projects/LoveBook';
+import { Progressive } from './Projects/Progressive';
+import { ShumunovSolutions } from './Projects/ShumunovSolutions';
 
 export function Projects({ activeTab }){
 	if (activeTab === 0){
 		return(
 			<div className="projects">
-				<div className="projects-grid">
-					<Card shadow={ 5 } className="project-cards">
-						<CardTitle className="project-cards-title  pomodoro" role="img" aria-label="Image of JavaScript Pomodoro clock built by Shugmi Shumunov">
-							<span className="title-background">
-								Pomodoro Clock
-							</span>
-						</CardTitle>
-						<CardText className="project-description">Built a Pomodoro clock in JavaScript that uses the pomodoro system and includes todo list functionality</CardText>
-						<CardActions border>
-							<Button colored>
-								<a href="https://github.com/ShugKnight24/pomodoro" rel="noopener noreferrer" target="_blank">GitHub Repository</a>
-							</Button>
-							<Button colored>
-								<a href="https://shugknight24.github.io/pomodoro" rel="noopener noreferrer" target="_blank">Live Demo</a>
-							</Button>
-						</CardActions>
-					</Card>
-					<Card shadow={ 5 } className="project-cards">
-						<CardTitle className="project-cards-title  mappr" role="img" aria-label="Image of JavaScript Mappr project built by Shugmi Shumunov">
-							<span className="title-background">
-								Mappr
-							</span>
-						</CardTitle>
-						<CardText className="project-description">Map project built using OpenStreet map and Leaflet.js - <span className="bold-text">Still in development</span></CardText>
-						<CardActions border>
-							<Button colored>
-								<a href="https://github.com/ShugKnight24/Mappr" rel="noopener noreferrer" target="_blank">GitHub Repository</a>
-							</Button>
-							<Button colored>
-								<a href="https://shugknight24.github.io/Mappr" rel="noopener noreferrer" target="_blank">Live Demo</a>
-							</Button>
-						</CardActions>
-					</Card>
-					<Card shadow={ 5 } className="project-cards">
-						<CardTitle className="project-cards-title  quizzlr" role="img" aria-label="Image of JavaScript Quizzlr project built by Shugmi Shumunov">
-							<span className="title-background">
-								Quizzlr
-							</span>
-						</CardTitle>
-						<CardText className="project-description">Quiz project built using jQuery. Custom modal system - <span className="bold-text">Still in development</span></CardText>
-						<CardActions border>
-							<Button colored>
-								{/* <a href="https://github.com/ShugKnight24/Dooer" rel="noopener noreferrer" target="_blank">GitHub Repository</a> */}
-							</Button>
-							<Button colored>
-								<a href="https://shumunovsolutions.com/quiz" rel="noopener noreferrer" target="_blank">Live Demo</a>
-							</Button>
-						</CardActions>
-					</Card>
-					<Card shadow={ 5 } className="project-cards">
-						<CardTitle className="project-cards-title  dooer" role="img" aria-label="Image of JavaScript Quizzlr project built by Shugmi Shumunov">
-							<span className="title-background">
-								Dooer
-							</span>
-						</CardTitle>
-						<CardText className="project-description">To-Do App built in React JS - Features a custom kanban board, basic to-do app, and pomodoro timer - <span className="bold-text">Still in development</span></CardText>
-						<CardActions border>
-							<Button colored>
-								{/* <a href="https://github.com/ShugKnight24/Quizzlr" rel="noopener noreferrer" target="_blank">GitHub Repository</a> */}
-							</Button>
-							<Button colored>
-								<a href="https://shumunovsolutions.com/dooer" rel="noopener noreferrer" target="_blank">Live Demo</a>
-							</Button>
-						</CardActions>
-					</Card>
-				</div>
+				<PersonalProjects />
 			</div>
 		)
 	}
@@ -77,24 +18,7 @@ export function Projects({ activeTab }){
 	if (activeTab === 1){
 		return(
 			<div className="projects">
-				<div className="projects-grid">
-					<Card shadow={ 5 } className="project-cards">
-						<CardTitle className="project-cards-title  " role="img" aria-label="">
-							<span className="title-background">
-								JS30 Projects will go here
-							</span>
-						</CardTitle>
-						<CardText className="project-description">JS30 Projects</CardText>
-						<CardActions border>
-							<Button colored>
-								<a href="https://github.com/ShugKnight24/" rel="noopener noreferrer" target="_blank">GitHub Repository</a>
-							</Button>
-							<Button colored>
-								<a href="https://shugknight24.github.io/" rel="noopener noreferrer" target="_blank">Live Demo</a>
-							</Button>
-						</CardActions>
-					</Card>
-				</div>
+				<JavaScript30 />
 			</div>
 		)
 	}
@@ -102,56 +26,7 @@ export function Projects({ activeTab }){
 	if (activeTab === 2){
 		return(
 			<div className="projects">
-				<div className="projects-grid">
-					<Card shadow={ 5 } className="project-cards">
-						<CardTitle className="project-cards-title  tetris" role="img" aria-label="Image of JavaScript Tetris clone built by Shugmi Shumunov">
-							<span className="title-background">
-								JavaScript Tetris Clone
-							</span>
-						</CardTitle>
-						<CardText className="project-description">Rebuilt Tetris with JavaScript and HTML5 Canvas</CardText>
-						<CardActions border>
-							<Button colored>
-								<a href="https://github.com/ShugKnight24/js_tetris/" rel="noopener noreferrer" target="_blank">GitHub Repository</a>
-							</Button>
-							<Button colored>
-								<a href="https://shugknight24.github.io/js_tetris/" rel="noopener noreferrer" target="_blank">Live Demo</a>
-							</Button>
-						</CardActions>
-					</Card>
-					<Card shadow={ 5 } className="project-cards">
-						<CardTitle className="project-cards-title flappy-bird" role="img" aria-label="Image of JavaScript Flappy Bird clone built by Shugmi Shumunov">
-							<span className="title-background">Flappy Bird Clone</span>
-						</CardTitle>
-						<CardText className="project-description">
-							Rebuilt Flappy Bird with JavaScript and HTML5 Canvas
-						</CardText>
-						<CardActions border>
-							<Button colored>
-								<a href="https://github.com/ShugKnight24/flappy_bird" rel="noopener noreferrer" target="_blank">GitHub Repository</a>
-							</Button>
-							<Button colored>
-								<a href="https://shugknight24.github.io/flappy_bird" rel="noopener noreferrer" target="_blank">Live Demo</a>
-							</Button>
-						</CardActions>
-					</Card>
-					<Card shadow={ 5 } className="project-cards">
-						<CardTitle className="project-cards-title trex-runner" role="img" aria-label="Image of T-Rex Runner Clone built by Shugmi Shumunov">
-							<span className="title-background">T-Rex Runner</span>
-						</CardTitle>
-						<CardText className="project-description">
-							Rebuilt Google Chrome Dino runner game w/ HTML, CSS, & Javascript
-						</CardText>
-						<CardActions border>
-							<Button colored>
-								<a href="https://github.com/ShugKnight24/dino_jump" rel="noopener noreferrer" target="_blank">GitHub Repository</a>
-							</Button>
-							<Button colored>
-								<a href="https://shugknight24.github.io/dino_jump" rel="noopener noreferrer" target="_blank">Live Demo</a>
-							</Button>
-						</CardActions>
-					</Card>
-				</div>
+				<Games />
 			</div>
 		)
 	}
@@ -159,83 +34,7 @@ export function Projects({ activeTab }){
 	if (activeTab === 3){
 		return(
 			<div className="projects">
-				<div className="projects-grid">
-					<Card shadow={ 5 } className="project-cards">
-						<CardTitle className="project-cards-title optima" role="img" aria-label="Image of Optima Batteries website homepage">
-							<span className="title-background">
-								Optima Batteries
-							</span>
-							</CardTitle>
-						<CardText>
-							Website built for Optima Batteries using Concrete5 CMS. Updated content, built features, and wrote styles as needed.
-						</CardText>
-						<CardActions border>
-							<Button colored>
-								<a href="https://www.optimabatteries.com/en-us" rel="noopener noreferrer" target="_blank">Live Site</a>
-							</Button>
-						</CardActions>
-					</Card>
-					<Card shadow={ 5 } className="project-cards">
-						<CardTitle className="project-cards-title varta" role="img" aria-label="Image of Varta Europe website homepage">
-							<span className="title-background">
-								Varta Europe
-							</span>
-							</CardTitle>
-						<CardText className="project-description">
-							Website built for Varta Europe using Concrete5 CMS. Updated content, built features, and wrote styles as needed.
-						</CardText>
-						<CardActions border>
-							<Button colored>
-								<a href="https://www.varta-automotive.com/en-gb" rel="noopener noreferrer" target="_blank">Live Site</a>
-							</Button>
-						</CardActions>
-					</Card>
-					<Card shadow={ 5 } className="project-cards">
-						<CardTitle className="project-cards-title varta-china" role="img" aria-label="Image of Varta China website homepage">
-							<span className="title-background">
-								Varta China
-							</span>
-							</CardTitle>
-						<CardText className="project-description">
-							Website built for Varta China using Concrete5 CMS. Needed their own site due to Chinese regulations. Updated content, built features, and wrote styles as needed.
-						</CardText>
-						<CardActions border>
-							<Button colored>
-								<a href="https://www.varta-automotive.cn/zh-cn" rel="noopener noreferrer" target="_blank">Live Site</a>
-							</Button>
-						</CardActions>
-					</Card>
-					<Card shadow={ 5 } className="project-cards">
-						<CardTitle className="project-cards-title delkor" role="img" aria-label="Image of Delkor Austrailia website homepage">
-							<span className="title-background">
-								Delkor Austrailia
-							</span>
-							</CardTitle>
-						<CardText className="project-description">
-							Website built for Delkor Austrailia using Concrete5 CMS. Updated content, built features, and wrote styles as needed.
-						</CardText>
-						<CardActions border>
-							<Button colored>
-								<a href="https://www.delkor.com.au/en-au" rel="noopener noreferrer" target="_blank">Live Site</a>
-							</Button>
-						</CardActions>
-					</Card>
-					<Card shadow={ 5 } className="project-cards">
-						<CardTitle className="project-cards-title delkor-korea" role="img" aria-label="Image of Delkor Korea website homepage">
-							<span className="title-background">
-								Delkor Korea
-							</span>
-							</CardTitle>
-						<CardText className="project-description">
-							Website built for Delkor Korea using Concrete5 CMS. Updated content, built features, and wrote styles as needed.
-						</CardText>
-						<CardActions border>
-							<Button colored>
-								<a href="https://www.delkor.kr/ko-kr" rel="noopener noreferrer" target="_blank">Live Site</a>
-							</Button>
-						</CardActions>
-					</Card>
-				</div>
+				<GTB />
 			</div>
 		)
 	}
@@ -243,38 +42,7 @@ export function Projects({ activeTab }){
 	if (activeTab === 4){
 		return(
 			<div className="projects">
-				<div className="projects-grid">
-					<Card shadow={ 5 } className="project-cards">
-						<CardTitle className="project-cards-title mylovebook" role="img" aria-label="Image of MyLoveBook website">
-							<span className="title-background">
-								MyLoveBook
-							</span>
-						</CardTitle>
-						<CardText className="project-description">
-							Redirect site for LoveBookOnline.com and LoveCoups.com
-						</CardText>
-						<CardActions border>
-							<Button colored>
-								<a href="https://www.mylovebook.com/" rel="noopener noreferrer" target="_blank">Live Site</a>
-							</Button>
-						</CardActions>
-					</Card>
-					<Card shadow={ 5 } className="project-cards">
-						<CardTitle className="project-cards-title lovecoups" role="img" aria-label="Image of LoveCoups website homepage">
-							<span className="title-background">
-								LoveCoups
-							</span>
-						</CardTitle>
-						<CardText className="project-description">
-							Build personalized coupon books for those you love
-						</CardText>
-						<CardActions border>
-							<Button colored>
-								<a href="https://lovecoups.com" rel="noopener noreferrer" target="_blank">Live Site</a>
-							</Button>
-						</CardActions>
-					</Card>
-				</div>
+				<LoveBook />
 			</div>
 		)
 	}
@@ -282,38 +50,7 @@ export function Projects({ activeTab }){
 	if (activeTab === 5){
 		return(
 			<div className="projects">
-				<div className="projects-grid">
-					<Card shadow={ 5 } className="project-cards">
-						<CardTitle className="project-cards-title stationeryhq" role="img" aria-label="Image of StationeryHQ website homepage">
-							<span className="title-background">
-								StationeryHQ
-							</span>
-						</CardTitle>
-						<CardText className="project-description">
-							Customized stationery made easy
-						</CardText>
-						<CardActions border>
-							<Button colored>
-								<a href="https://www.stationeryhq.com" rel="noopener noreferrer" target="_blank">Live Site</a>
-							</Button>
-						</CardActions>
-					</Card>
-					<Card shadow={ 5 } className="project-cards">
-						<CardTitle className="project-cards-title frecklebox" role="img" aria-label="Image of FreckleBox website homepage">
-							<span className="title-background">
-								FreckleBox
-							</span>
-						</CardTitle>
-						<CardText className="project-description">
-							Personalized gifts for kids
-						</CardText>
-						<CardActions border>
-							<Button colored>
-								<a href="https://www.frecklebox.com" rel="noopener noreferrer" target="_blank">Live Site</a>
-							</Button>
-						</CardActions>
-					</Card>
-				</div>
+				<Progressive />
 			</div>
 		)
 	}
@@ -321,53 +58,7 @@ export function Projects({ activeTab }){
 	if (activeTab === 6){
 		return(
 			<div className="projects">
-				<div className="projects-grid">
-					<Card shadow={ 5 } className="project-cards">
-						<CardTitle className="project-cards-title shumunov-solutions" role="img" aria-label="Image of Shumunov Solutions website homepage">
-							<span className="title-background">
-								Shumunov Solutions
-							</span>
-						</CardTitle>
-						<CardText className="project-description">
-							Providing technology solutions to South East Michigan businesses
-						</CardText>
-						<CardActions border>
-							<Button colored>
-								<a href="https://www.shumunovsolutions.com" rel="noopener noreferrer" target="_blank">Live Site</a>
-							</Button>
-						</CardActions>
-					</Card>
-					<Card shadow={ 5 } className="project-cards">
-						<CardTitle className="project-cards-title jk-unlimited-services" role="img" aria-label="Image of JK Unlimited Services website homepage">
-							<span className="title-background">
-								JK Unlimited Services
-							</span>
-						</CardTitle>
-						<CardText className="project-description">
-							Website for West Bloomfield, MI general contractor
-						</CardText>
-						<CardActions border>
-							<Button colored>
-								<a href="https://www.jkunlimitedservices.com/" rel="noopener noreferrer" target="_blank">Live Site</a>
-							</Button>
-						</CardActions>
-					</Card>
-					<Card shadow={ 5 } className="project-cards">
-						<CardTitle className="project-cards-title paper-goods-warehouse" role="img" aria-label="Image of Paper Goods Warehouse website homepage">
-							<span className="title-background">
-								Paper Goods Warehouse
-							</span>
-						</CardTitle>
-						<CardText className="project-description">
-							Website for Oak Park, MI Paper Goods Store
-						</CardText>
-						<CardActions border>
-							<Button colored>
-								<a href="https://www.papergoodswarehouse.com/" rel="noopener noreferrer" target="_blank">Live Site</a>
-							</Button>
-						</CardActions>
-					</Card>
-				</div>
+				<ShumunovSolutions />
 			</div>
 		)
 	}
