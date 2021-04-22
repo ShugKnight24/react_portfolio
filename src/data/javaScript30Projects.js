fe091ba44f87f7abc742ecfa6149e256f9d87200
Shugmi Shumunov <SShumunov@gmail.com>
2021-04-22 09:19:50 -0400

    Add js30 projects dataset

---
+++ src/data/javaScript30Projects.js
@@ -0,0 +1,212 @@
+export const javaScript30Projects = [
+	{
+		title: 'Day 1 - Drum Kit',
+		projectDescription: '',
+		githubLink: 'https://github.com/ShugKnight24/javascript_30/tree/master/1_drum_kit',
+		projectLink: 'https://shumunovsolutions.com/js30/1_drum_kit/',
+		cssClass: 'drum-kit'
+	},
+	{
+		title: 'Day 2 - CSS and JS Clock',
+		projectDescription: '',
+		githubLink: 'https://github.com/ShugKnight24/javascript_30/tree/master/2_css_js_clock',
+		projectLink: 'https://shumunovsolutions.com/js30/2_css_js_clock',
+		cssClass: 'css-js-clock'
+	},
+	{
+		title: 'Day 3 - Updating CSS variables with JS',
+		projectDescription: '',
+		githubLink: 'https://github.com/ShugKnight24/javascript_30/tree/master/3_css_variables_js',
+		projectLink: 'https://shumunovsolutions.com/js30/3_css_variables_js',
+		cssClass: 'css-js-vars'
+	},
+	{
+		title: 'Day 4 - Array Cardio',
+		projectDescription: '',
+		githubLink: 'https://github.com/ShugKnight24/javascript_30/tree/master/4_array_cardio',
+		projectLink: 'https://shumunovsolutions.com/js30/4_array_cardio',
+		cssClass: 'array-cardio-1'
+	},
+	{
+		title: 'Day 5 - Flex Panels Image Gallery',
+		projectDescription: '',
+		githubLink: 'https://github.com/ShugKnight24/javascript_30/tree/master/5_flex_panel_gallery',
+		projectLink: 'https://shumunovsolutions.com/js30/5_flex_panel_gallery',
+		cssClass: 'flex-panels'
+	},
+	{
+		title: 'Day 6 - Ajax Type Ahead',
+		projectDescription: '',
+		githubLink: 'https://github.com/ShugKnight24/javascript_30/tree/master/6_ajax_type_ahead',
+		projectLink: 'https://shumunovsolutions.com/js30/6_ajax_type_ahead',
+		cssClass: 'ajax-map'
+	},
+	{
+		title: 'Day 7 - Array Cardio Day 2',
+		projectDescription: '',
+		githubLink: 'https://github.com/ShugKnight24/javascript_30/tree/master/7_array_cardio_2',
+		projectLink: 'https://shumunovsolutions.com/js30/7_array_cardio_2',
+		cssClass: 'array-cardio-2'
+	},
+	{
+		title: 'Day 8 - Fun with HTML 5 Canvas',
+		projectDescription: '',
+		githubLink: 'https://github.com/ShugKnight24/javascript_30/tree/master/8_html5_canvas',
+		projectLink: 'https://shumunovsolutions.com/js30/8_html5_canvas',
+		cssClass: 'canvas-drawing'
+	},
+	{
+		title: 'Day 9 - 14 Must Know Dev Tools Tricks',
+		projectDescription: '',
+		githubLink: 'https://github.com/ShugKnight24/javascript_30/tree/master/9_dev_tools',
+		projectLink: 'https://shumunovsolutions.com/js30/9_dev_tools',
+		cssClass: 'dev-tool-tricks'
+	},
+	{
+		title: 'Day 10 - Hold Shift to Check Multiple Checkboxes',
+		projectDescription: '',
+		githubLink: 'https://github.com/ShugKnight24/javascript_30/tree/master/10_check_multiple_boxes',
+		projectLink: 'https://shumunovsolutions.com/js30/10_check_multiple_boxes',
+		cssClass: 'multiple-checkboxes'
+	},
+	{
+		title: 'Day 11 - Custom HTML5 Video Player',
+		projectDescription: '',
+		githubLink: 'https://github.com/ShugKnight24/javascript_30/tree/master/11_custom_video_player',
+		projectLink: 'https://shumunovsolutions.com/js30/11_custom_video_player',
+		cssClass: 'html5-video'
+	},
+	{
+		title: 'Day 12 - Key Sequence Detection',
+		projectDescription: '',
+		githubLink: 'https://github.com/ShugKnight24/javascript_30/tree/master/12_key_sequence_detection',
+		projectLink: 'https://shumunovsolutions.com/js30/12_key_sequence_detection',
+		cssClass: 'key-sequence-detection'
+	},
+	{
+		title: 'Day 13 - Slide In On Scroll',
+		projectDescription: '',
+		githubLink: 'https://github.com/ShugKnight24/javascript_30/tree/master/13_slide_in_scroll',
+		projectLink: 'https://shumunovsolutions.com/js30/13_slide_in_scroll',
+		cssClass: 'slide-image-on-scroll'
+	},
+	{
+		title: 'Day 14 - Object and Arrays - References vs Copying',
+		projectDescription: '',
+		githubLink: 'https://github.com/ShugKnight24/javascript_30/tree/master/14_references_vs_copying',
+		projectLink: 'https://shumunovsolutions.com/js30/14_references_vs_copying',
+		cssClass: 'references-vs-copying'
+	},
+	{
+		title: 'Day 15 - LocalStorage and Event Delegation',
+		projectDescription: '',
+		githubLink: 'https://github.com/ShugKnight24/javascript_30/tree/master/15_localstorage',
+		projectLink: 'https://shumunovsolutions.com/js30/15_localstorage',
+		cssClass: 'local-storage'
+	},
+	{
+		title: 'Day 16 - CSS Text Shadow Mouse Move Effect',
+		projectDescription: '',
+		githubLink: 'https://github.com/ShugKnight24/javascript_30/tree/master/16_mouse_move_shadow',
+		projectLink: 'https://shumunovsolutions.com/js30/16_mouse_move_shadow',
+		cssClass: 'css-text-shadow'
+	},
+	{
+		title: 'Day 17 - Sorting Band Names Without Articles',
+		projectDescription: '',
+		githubLink: 'https://github.com/ShugKnight24/javascript_30/tree/master/17_array_sort',
+		projectLink: 'https://shumunovsolutions.com/js30/17_array_sort',
+		cssClass: 'sorting-data'
+	},
+	{
+		title: 'Day 18 - Tally String Times with Reduce',
+		projectDescription: '',
+		githubLink: 'https://github.com/ShugKnight24/javascript_30/tree/master/18_tally_string_times',
+		projectLink: 'https://shumunovsolutions.com/js30/18_tally_string_times',
+		cssClass: 'tally-times'
+	},
+	{
+		title: 'Day 19 - Unreal Webcam Fun',
+		projectDescription: '',
+		githubLink: 'https://github.com/ShugKnight24/javascript_30/tree/master/19_webcam_fun',
+		projectLink: 'https://shumunovsolutions.com/js30/19_webcam_fun',
+		cssClass: 'webcam-fun'
+	},
+	{
+		title: 'Day 20 - Native Speech Recognition',
+		projectDescription: '',
+		githubLink: 'https://github.com/ShugKnight24/javascript_30/tree/master/20_speech_recognition',
+		projectLink: 'https://shumunovsolutions.com/js30/20_speech_recognition',
+		cssClass: 'speech-recognition'
+	},
+	{
+		title: 'Day 21 - Geolocation Based Speedometer and Compass',
+		projectDescription: '',
+		githubLink: 'https://github.com/ShugKnight24/javascript_30/tree/master/21_geolocation',
+		projectLink: 'https://shumunovsolutions.com/js30/21_geolocation',
+		cssClass: 'geolocation'
+	},
+	{
+		title: 'Day 22 - Follow Along Links',
+		projectDescription: '',
+		githubLink: 'https://github.com/ShugKnight24/javascript_30/tree/master/22_follow_along_links',
+		projectLink: 'https://shumunovsolutions.com/js30/22_follow_along_links',
+		cssClass: 'follow-along-links'
+	},
+	{
+		title: 'Day 23 - Speech Synthesis',
+		projectDescription: '',
+		githubLink: 'https://github.com/ShugKnight24/javascript_30/tree/master/23_speech_synthesis',
+		projectLink: 'https://shumunovsolutions.com/js30/23_speech_synthesis',
+		cssClass: 'speech-synthesis'
+	},
+	{
+		title: 'Day 24 - Sticky Nav',
+		projectDescription: '',
+		githubLink: 'https://github.com/ShugKnight24/javascript_30/tree/master/24_sticky_nav',
+		projectLink: 'https://shumunovsolutions.com/js30/24_sticky_nav',
+		cssClass: 'sticky-nav'
+	},
+	{
+		title: 'Day 25 - Event Capture, Propagation, Bubbling and Once',
+		projectDescription: '',
+		githubLink: 'https://github.com/ShugKnight24/javascript_30/tree/master/25_event_capture',
+		projectLink: 'https://shumunovsolutions.com/js30/25_event_capture',
+		cssClass: 'event-capture'
+	},
+	{
+		title: 'Day 26 - Follow Along Dropdown',
+		projectDescription: '',
+		githubLink: 'https://github.com/ShugKnight24/javascript_30/tree/master/26_follow_along_dropdown',
+		projectLink: 'https://shumunovsolutions.com/js30/26_follow_along_dropdown',
+		cssClass: 'follow-dropdowns'
+	},
+	{
+		title: 'Day 27 - Click and Drag Scroll',
+		projectDescription: '',
+		githubLink: 'https://github.com/ShugKnight24/javascript_30/tree/master/27_click_drag_scroll',
+		projectLink: 'https://shumunovsolutions.com/js30/27_click_drag_scroll',
+		cssClass: 'click-drag-scroll'
+	},
+	{
+		title: 'Day 28 - Video Speed Controller',
+		projectDescription: '',
+		githubLink: 'https://github.com/ShugKnight24/javascript_30/tree/master/28_video_speed_controller',
+		projectLink: 'https://shumunovsolutions.com/js30/28_video_speed_controller',
+		cssClass: 'video-speed'
+	},
+	{
+		title: 'Day 29 - Countdown Timer',
+		projectDescription: '',
+		githubLink: 'https://github.com/ShugKnight24/javascript_30/tree/master/29_countdown_timer',
+		projectLink: 'https://shumunovsolutions.com/js30/29_countdown_timer',
+		cssClass: 'countdown-timer'
+	},
+	{
+		title: 'Day 30 - Whack-A-Mole',
+		projectDescription: '',
+		githubLink: 'https://github.com/ShugKnight24/javascript_30/tree/master/30_whack_a_mole',
+		projectLink: 'https://shumunovsolutions.com/js30/30_whack_a_mole',
+		cssClass: 'whack-a-mole'
+	},
+];
