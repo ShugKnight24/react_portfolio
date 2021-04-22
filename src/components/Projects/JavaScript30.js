16e40f4a8ece7b6e0925684eb9881684b3f03932
Shugmi Shumunov <SShumunov@gmail.com>
2021-04-22 09:20:09 -0400

    Build js30 project components

--- src/components/Projects/JavaScript30.js
+++ src/components/Projects/JavaScript30.js
@@ -1,24 +1,33 @@
 import { Card, CardTitle, CardText, CardActions, Button } from 'react-mdl';
 
+import { javaScript30Projects } from '../../data/javaScript30Projects';
+
 export function JavaScript30(){
 	return(
 		<div className="projects-grid">
-			<Card shadow={ 5 } className="project-cards">
-				<CardTitle className="project-cards-title  " role="img" aria-label="">
-					<span className="title-background">
-						JS30 Projects will go here
-					</span>
-				</CardTitle>
-				<CardText className="project-description">JS30 Projects</CardText>
-				<CardActions border>
-					<Button colored>
-						<a href="https://github.com/ShugKnight24/" rel="noopener noreferrer" target="_blank">GitHub Repository</a>
-					</Button>
-					<Button colored>
-						<a href="https://shugknight24.github.io/" rel="noopener noreferrer" target="_blank">Live Demo</a>
-					</Button>
-				</CardActions>
-			</Card>
+			{
+				javaScript30Projects.map(({ title, projectDescription, githubLink, projectLink, cssClass }, index) => {
+					return(
+						<Card shadow={ 5 } className="project-cards" key={ title }>
+							<CardTitle className={`project-cards-title ${ cssClass }`} role="img" aria-label={ title }>
+								<span className="title-background">
+									{ title }
+								</span>
+							</CardTitle>
+							<CardText className="project-description">{ projectDescription }</CardText>
+							<CardActions border>
+								<Button colored>
+									<a href={ githubLink } rel="noopener noreferrer" target="_blank">GitHub Repository</a>
+								</Button>
+								<Button colored>
+									<a href={ projectLink } rel="noopener noreferrer" target="_blank">Live Demo</a>
+								</Button>
+							</CardActions>
+						</Card>
+					)
+				})
+			}
+			
 		</div>
 	)
 }
