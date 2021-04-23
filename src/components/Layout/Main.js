c8b72fbdc83a90d6f9aa05bfce199ae4a2f3c43f
Shugmi Shumunov <SShumunov@gmail.com>
2021-04-23 02:14:25 -0400

    Update ContactPage Component

--- src/components/Layout/Main.js
+++ src/components/Layout/Main.js
@@ -4,7 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import LandingPage from '../Pages/Landingpage';
 import { AboutPage } from '../Pages/AboutPage';
 import BooksPage from '../BooksPage';
-import Contact from '../Contact';
+import { ContactPage } from '../Pages/ContactPage';
 import PhotoGal from '../Pages/PhotoGal';
 import { ProjectsPage } from '../Pages/ProjectsPage';
 import Resume from '../Resume';
@@ -15,7 +15,7 @@ const Main = () => (
 		<Route exact path="/" component={ LandingPage } />
 		<Route path="/about" component={ AboutPage } />
 		<Route path="/books" component={ BooksPage } />
-		<Route path="/contact" component={ Contact } />
+		<Route path="/contact" component={ ContactPage } />
 		<Route path="/photos" component={ PhotoGal } />
 		<Route path="/projects" component={ ProjectsPage } />
 		<Route path="/resume" component={ Resume } />
---
+++ src/components/Pages/ContactPage.js
@@ -0,0 +1,68 @@
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
+
+export function ContactPage(){
+	return(
+		<div className="contact-body">
+			<Grid className="contact-grid">
+				<Cell col={ 6 }>
+					<div className="image-container">
+						<figure>
+							<img
+								className="jbp-image"
+								src="./img/jbp_shug.jpg"
+								alt="Shugmi Shumunov with Jordan Peterson"
+							/>
+							<figcaption>
+								With author, clinical psychologist, lecturer, & professor <a href="https://www.jordanbpeterson.com/" rel="noopener noreferrer" target="_blank">Jordan B. Peterson</a>
+							</figcaption>
+						</figure>
+					</div>
+					<h3>Shugmi Shumunov</h3>
+					<h6>
+						Telling stories through code
+					</h6>
+					<p className="tab">
+						Whether you're exploring new opportunities in the expanding digital marketplace or your development needs have fallen short, I step in to provide insights and deliver a creative solutions that bridge the gap between concept and reality.
+					</p>
+					<p className="tab">
+						I provide consulting, development, development mentorship, management, and project management services.
+					</p>
+					<p className="tab">
+						I pride myself on providing honest, insightful, and thoughtful guidance to my clients
+					</p>
+					<p className="tab">
+						Reach out if you'd like to work with me.
+					</p>
+				</Cell>
+				<Cell col={ 6 }>
+					<h3 className="centered">Contact Info</h3>
+					<hr />
+					<div className="contact-list">
+						<List>
+							<ListItem>
+								<ListItemContent>
+									<i className="fa fa-envelope-square" aria-hidden="true"/>
+									SShumunov @ Google's email
+								</ListItemContent>
+							</ListItem>
+							<ListItem>
+								<ListItemContent>
+								<a className="github-icon" href="https://github.com/ShugKnight24" rel="noopener noreferrer" target="_blank">
+									<i class="fab fa-github-square" aria-hidden="true"></i>GitHub
+								</a>
+								</ListItemContent>
+							</ListItem>
+							<ListItem>
+								<ListItemContent>
+									<a className="linkedin-icon" href="https://www.linkedin.com/in/shugmishumunov/" rel="noopener noreferrer" target="_blank">
+										<i className="fab fa-linkedin" aria-hidden="true" />LinkedIn
+									</a>
+								</ListItemContent>
+							</ListItem>
+						</List>
+					</div>
+				</Cell>
+			</Grid>
+		</div>
+	);
+}
