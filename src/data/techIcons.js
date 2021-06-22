90e4129ded8f6cd2094a81aa15307f7ca7c3d7df
Shugmi Shumunov <SShumunov@gmail.com>
2021-06-22 19:39:33 -0400

    Start refactor of techIcons
    - Think of better approach

--- src/data/techIcons.js
+++ src/data/techIcons.js
@@ -1,70 +1,116 @@
 export const techIcons = [
 	{
 		iconName: 'HTML',
-		iconURL: '../img/tech_icons/html.svg'
+		iconURL: '../img/tech_icons/html.svg',
+		progress: 95,
+		skillType: 'Language'
 	},
 	{
 		iconName: 'CSS',
-		iconURL: '../img/tech_icons/css.svg'
+		iconURL: '../img/tech_icons/css.svg',
+		progress: 85,
+		skillType: 'Language'
 	},
 	{
 		iconName: 'SASS',
-		iconURL: '../img/tech_icons/sass.svg'
+		iconURL: '../img/tech_icons/sass.svg',
+		progress: 85,
+		skillType: 'Language'
 	},
 	{
 		iconName: 'BootStrap',
-		iconURL: '../img/tech_icons/bootstrap.svg'
+		iconURL: '../img/tech_icons/bootstrap.svg',
+		progress: 75,
+		skillType: 'Front-End Framework / Library'
 	},
 	{
 		iconName: 'JavaScript',
-		iconURL: '../img/tech_icons/javascript.svg'
+		iconURL: '../img/tech_icons/javascript.svg',
+		progress: 85,
+		skillType: 'Language'
 	},
 	{
 		iconName: 'TypeScript',
-		iconURL: '../img/tech_icons/typescript.svg'
+		iconURL: '../img/tech_icons/typescript.svg',
+		progress: 75,
+		skillType: 'Language'
 	},
 	{
 		iconName: 'jQuery',
-		iconURL: '../img/tech_icons/jquery.svg'
+		iconURL: '../img/tech_icons/jquery.svg',
+		progress: 80,
+		skillType: 'Front-End Framework / Library'
 	},
 	{
 		iconName: 'React',
-		iconURL: '../img/tech_icons/react.svg'
+		iconURL: '../img/tech_icons/react.svg',
+		progress: 85,
+		skillType: 'Front-End Framework / Library'
 	},
 	{
 		iconName: 'React Native',
-		iconURL: '../img/tech_icons/react.svg'
+		iconURL: '../img/tech_icons/react.svg',
+		progress: 75,
+		skillType: 'Front-End Framework / Library'
 	},
 	{
 		iconName: 'NodeJS',
-		iconURL: '../img/tech_icons/nodejs.svg'
+		iconURL: '../img/tech_icons/nodejs.svg',
+		progress: 70,
+		skillType: 'Back-End Framework / Library'
 	},
 	{
 		iconName: 'D3',
-		iconURL: '../img/tech_icons/d3.svg'
+		iconURL: '../img/tech_icons/d3.svg',
+		progress: 60,
+		skillType: 'Front-End Framework / Library'
 	},
 	{
 		iconName: 'PHP',
-		iconURL: '../img/tech_icons/php.svg'
+		iconURL: '../img/tech_icons/php.svg',
+		progress: 75,
+		skillType: 'Language'
 	},
 	{
 		iconName: 'MySQL',
-		iconURL: '../img/tech_icons/mysql.svg'
+		iconURL: '../img/tech_icons/mysql.svg',
+		progress: 45,
+		skillType: 'Database'
+	},
+	{
+		iconName: 'MongoDB',
+		iconURL: '../img/tech_icons/mongodb.svg',
+		progress: 30,
+		skillType: 'Database'
 	},
 	{
 		iconName: 'Shopify',
-		iconURL: '../img/tech_icons/shopify.svg'
+		iconURL: '../img/tech_icons/shopify.svg',
+		progress: 80,
+		skillType: 'Tool'
 	},
 	{
 		iconName: 'Shopify Liquid',
-		iconURL: '../img/tech_icons/shopify_liquid.svg'
+		iconURL: '../img/tech_icons/shopify_liquid.svg',
+		progress: 90,
+		skillType: 'Tool'
 	},
 	{
 		iconName: 'Rust',
-		iconURL: '../img/tech_icons/rust.svg'
+		iconURL: '../img/tech_icons/rust.svg',
+		progress: 95,
+		skillType: 'Language'
 	},
 	{
 		iconName: 'Python',
-		iconURL: '../img/tech_icons/python.svg'
+		iconURL: '../img/tech_icons/python.svg',
+		progress: 95,
+		skillType: 'Language'
+	},
+	{
+		iconName: 'Git',
+		iconURL: '../img/tech_icons/git.svg',
+		progress: 75,
+		skillType: 'Tool'
 	}
 ];
