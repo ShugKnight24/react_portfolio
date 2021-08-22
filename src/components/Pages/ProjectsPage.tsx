77862bd84e43dac22eea870d5787b99ffff1e55b
Shugmi Shumunov <SShumunov@gmail.com>
2021-08-22 13:49:01 -0400

    refactor: Remove 'react-mdl' Tabs and replace w/ Custom Tabs
    - Update styles accordingly

--- src/components/Pages/ProjectsPage.tsx
+++ src/components/Pages/ProjectsPage.tsx
@@ -1,6 +1,6 @@
 import { FC, useState } from 'react';
-import { Tabs, Tab } from 'react-mdl';
 import { Cell, Grid } from '../Grid';
+import { Tabs, Tab } from '../Tabs';
 
 import { Projects } from '../Projects';
 
@@ -10,36 +10,29 @@ export const ProjectsPage: FC = () => {
 	return(
 		<div className="projects-page">
 			<Tabs
-				className="projects-tabs"
-				ripple
+				extraClass="projects-tabs"
+				activeTab={activeTab}
 			>
 				<Tab
-					className="tab-name"
-					onClick={ () => setActiveTab( 0 ) }
+					updateActiveTab={ () => setActiveTab(0) }
 				>Personal Projects</Tab>
 				<Tab
-					className="tab-name"
-					onClick={ () => setActiveTab( 1 ) }
+					updateActiveTab={ () => setActiveTab(1) }
 				>JS30</Tab>
 				<Tab
-					className="tab-name"
-					onClick={ () => setActiveTab( 2 ) }
+					updateActiveTab={ () => setActiveTab(2) }
 				>Games</Tab>
 				<Tab
-					className="tab-name"
-					onClick={ () => setActiveTab( 3 ) }
+					updateActiveTab={ () => setActiveTab(3) }
 				>GTB</Tab>
 				<Tab
-					className="tab-name"
-					onClick={ () => setActiveTab( 4 ) }
+					updateActiveTab={ () => setActiveTab(4) }
 				>LoveBook</Tab>
 				<Tab
-					className="tab-name"
-					onClick={ () => setActiveTab( 5 ) }
+					updateActiveTab={ () => setActiveTab(5) }
 				>Progressive Solutions</Tab>
 				<Tab
-					className="tab-name"
-					onClick={ () => setActiveTab( 6 ) }
+					updateActiveTab={ () => setActiveTab(6) }
 				>Shumunov Solutions</Tab>
 			</Tabs>
 			<Grid>
