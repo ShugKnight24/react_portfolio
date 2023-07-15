9b0322b0198c4738a6a360ad8443a318060dbbc9
Shugmi Shumunov <SShumunov@gmail.com>
2023-07-14 22:39:02 -0400

    chore: Make footer year currently dynamically

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,9 +1,11 @@
 import { FC } from 'react';
 
 export const Footer: FC = () => {
+	const currentYear = new Date().getFullYear();
+
 	return(
 		<div className="footer">
-			<p>Built by Shugmi Shumunov &copy; 2023. All Rights Reserved</p>
+			<p>Built by Shugmi Shumunov &copy; { currentYear }. All Rights Reserved</p>
 		</div>
 	)
 }
