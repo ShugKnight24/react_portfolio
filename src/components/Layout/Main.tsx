30e0b710cbdb81eb73b0d31794d19ea1ca58b831
Shugmi Shumunov <SShumunov@gmail.com>
2023-12-21 21:18:20 -0500

    refactor: Update to use React Lazy loading

--- src/components/Layout/Main.tsx
+++ src/components/Layout/Main.tsx
@@ -1,26 +1,26 @@
-import { FC } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { FC, lazy } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import {
-	AboutPage,
-	BooksPage,
-	ContactPage,
-	LandingPage,
-	NotFoundPage,
-	PhotoGal,
-	ProjectsPage,
-	ResumePage
-} from '../Pages';
+const AboutPage = lazy(() => import('../Pages').then(module => ({ default: module.AboutPage })));
+const BooksPage = lazy(() => import('../Pages').then(module => ({ default: module.BooksPage })));
+const ContactPage = lazy(() => import('../Pages').then(module => ({ default: module.ContactPage })));
+const LandingPage = lazy(() => import('../Pages').then(module => ({ default: module.LandingPage })));
+const NotFoundPage = lazy(() => import('../Pages').then(module => ({ default: module.NotFoundPage })));
+const PhotoGal = lazy(() => import('../Pages').then(module => ({ default: module.PhotoGal })));
+const ProjectsPage = lazy(() => import('../Pages').then(module => ({ default: module.ProjectsPage })));
+const ResumePage = lazy(() => import('../Pages').then(module => ({ default: module.ResumePage })));
 
 export const Main: FC = () => (
-	<Switch>
-		<Route exact path="/" component={ LandingPage } />
-		<Route path="/about" component={ AboutPage } />
-		<Route path="/books" component={ BooksPage } />
-		<Route path="/contact" component={ ContactPage } />
-		<Route path="/photos" component={ PhotoGal } />
-		<Route path="/projects" component={ ProjectsPage } />
-		<Route path="/resume" component={ ResumePage } />
-		<Route path="*" component={ NotFoundPage } />
-	</Switch>
+	<BrowserRouter>
+		<Routes>
+			<Route path="/" element={ <LandingPage /> } />
+			<Route path="/about" element={ <AboutPage /> } />
+			<Route path="/books" element={ <BooksPage /> } />
+			<Route path="/contact" element={ <ContactPage /> } />
+			<Route path="/photos" element={ <PhotoGal /> } />
+			<Route path="/projects" element={ <ProjectsPage /> } />
+			<Route path="/resume" element={ <ResumePage /> } />
+			<Route path="*" element={ <NotFoundPage /> } />
+		</Routes>
+	</BrowserRouter>
 );
