74f0b3ca469da1fe9a49af845d47afe06142267e
Shugmi Shumunov <SShumunov@gmail.com>
2023-12-31 22:47:42 -0500

    feat: Create `ProjectCard` component to DRY out project pages

--- src/types/projects.ts
+++ src/types/projects.ts
@@ -1,3 +1,13 @@
 export type ActiveTab = {
 	activeTab: number;
 }
+
+export interface ProjectCardProps {
+  titleAriaLabel: string;
+  titleExtraClass: string;
+  titleRole: string;
+  title: string;
+  description?: string[];
+  buttonLinks?: string[];
+  buttonText?: string[];
+}
