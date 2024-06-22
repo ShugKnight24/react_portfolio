3477d08fd52982ebfa7d02b5a40026f7978dfe9c
Shugmi Shumunov <SShumunov@gmail.com>
2024-06-22 02:11:40 -0400

    update: Add Vue & Pinia svgs
    - `techIcons.js` autoformatting

--- src/data/techIcons.js
+++ src/data/techIcons.js
@@ -1,139 +1,161 @@
 export const techIcons = [
-	{
-		iconName: 'JavaScript',
-		iconURL: '../img/tech_icons/javascript.svg',
-		progress: 85,
-		skillType: 'Language',
-		nameInLogo: false
-	},
-	{
-		iconName: 'TypeScript',
-		iconURL: '../img/tech_icons/typescript.svg',
-		progress: 75,
-		skillType: 'Language',
-		nameInLogo: false
-	},
-	{
-		iconName: 'React & React Native',
-		iconURL: '../img/tech_icons/react.svg',
-		progress: 85,
-		skillType: 'Front-End Framework / Library',
-		nameInLogo: false
-	},
-	{
-		iconName: 'NextJS',
-		iconURL: '../img/tech_icons/nextjs.svg',
-		progress: 75,
-		skillType: 'Front-End Framework / Library',
-		nameInLogo: true
-	},
-	{
-		iconName: 'NodeJS',
-		iconURL: '../img/tech_icons/nodejs.svg',
-		progress: 70,
-		skillType: 'Back-End Framework / Library',
-		nameInLogo: true
-	},
-	{
-		iconName: 'HTML',
-		iconURL: '../img/tech_icons/html.svg',
-		progress: 95,
-		skillType: 'Language',
-		nameInLogo: true
-	},
-	{
-		iconName: 'CSS',
-		iconURL: '../img/tech_icons/css.svg',
-		progress: 85,
-		skillType: 'Language',
-		nameInLogo: true
-	},
-	{
-		iconName: 'TailwindCSS',
-		iconURL: '../img/tech_icons/tailwindcss.svg',
-		progress: 60,
-		skillType: 'CSS framework',
-		nameInLogo: true
-	},
-	{
-		iconName: 'SASS',
-		iconURL: '../img/tech_icons/sass.svg',
-		progress: 85,
-		skillType: 'Language',
-		nameInLogo: true
-	},
-	{
-		iconName: 'D3',
-		iconURL: '../img/tech_icons/d3.svg',
-		progress: 60,
-		skillType: 'Front-End Framework / Library',
-		nameInLogo: true
-	},
-	{
-		iconName: 'PHP',
-		iconURL: '../img/tech_icons/php.svg',
-		progress: 60,
-		skillType: 'Language',
-		nameInLogo: true
-	},
-	{
-		iconName: 'MySQL',
-		iconURL: '../img/tech_icons/mysql.svg',
-		progress: 45,
-		skillType: 'Database',
-		nameInLogo: true
-	},
-	{
-		iconName: 'MongoDB',
-		iconURL: '../img/tech_icons/mongodb.svg',
-		progress: 30,
-		skillType: 'Database',
-		nameInLogo: true
-	},
-	{
-		iconName: 'GraphQL',
-		iconURL: '../img/tech_icons/graphql.svg',
-		progress: 30,
-		skillType: 'API',
-		nameInLogo: false
-	},
-	{
-		iconName: 'Python',
-		iconURL: '../img/tech_icons/python.svg',
-		progress: 60,
-		skillType: 'Language',
-		nameInLogo: false
-	},
-	{
-		iconName: 'Git',
-		iconURL: '../img/tech_icons/git.svg',
-		progress: 75,
-		skillType: 'Tool',
-		nameInLogo: true
-	},
-	{
-		iconName: 'Swift',
-		iconURL: '../img/tech_icons/swift.svg',
-		progress: 30,
-		skillType: 'Language',
-		nameInLogo: false
-	},
-	{
-		iconName: 'SwiftUI',
-		iconURL: '../img/tech_icons/swiftui.svg',
-		progress: 30,
-		skillType: 'UI Framework',
-		nameInLogo: false
-	},
-	{
-		iconName: 'jQuery',
-		iconURL: '../img/tech_icons/jquery.svg',
-		progress: 80,
-		skillType: 'Front-End Framework / Library',
-		nameInLogo: true
-	}
+  {
+    iconName: 'JavaScript',
+    iconURL: '../img/tech_icons/javascript.svg',
+    progress: 85,
+    skillType: 'Language',
+    nameInLogo: false,
+  },
+  {
+    iconName: 'TypeScript',
+    iconURL: '../img/tech_icons/typescript.svg',
+    progress: 75,
+    skillType: 'Language',
+    nameInLogo: false,
+  },
+  {
+    iconName: 'React & React Native',
+    iconURL: '../img/tech_icons/react.svg',
+    progress: 85,
+    skillType: 'Front-End Framework / Library',
+    nameInLogo: false,
+  },
+  {
+    iconName: 'VueJS',
+    iconURL: '../img/tech_icons/vue.svg',
+    progress: 30,
+    skillType: 'Front-End Framework / Library',
+    nameInLogo: false,
+  },
+  {
+    iconName: 'Pinia',
+    iconURL: '../img/tech_icons/pinia.svg',
+    progress: 30,
+    skillType: 'Vue State Management Library',
+    nameInLogo: false,
+  },
+  {
+    iconName: 'NextJS',
+    iconURL: '../img/tech_icons/nextjs.svg',
+    progress: 75,
+    skillType: 'Front-End Framework / Library',
+    nameInLogo: true,
+  },
+  {
+    iconName: 'NodeJS',
+    iconURL: '../img/tech_icons/nodejs.svg',
+    progress: 70,
+    skillType: 'Back-End Framework / Library',
+    nameInLogo: true,
+  },
+  {
+    iconName: 'HTML',
+    iconURL: '../img/tech_icons/html.svg',
+    progress: 95,
+    skillType: 'Language',
+    nameInLogo: true,
+  },
+  {
+    iconName: 'CSS',
+    iconURL: '../img/tech_icons/css.svg',
+    progress: 85,
+    skillType: 'Language',
+    nameInLogo: true,
+  },
+  {
+    iconName: 'TailwindCSS',
+    iconURL: '../img/tech_icons/tailwindcss.svg',
+    progress: 60,
+    skillType: 'CSS framework',
+    nameInLogo: true,
+  },
+  {
+    iconName: 'SASS',
+    iconURL: '../img/tech_icons/sass.svg',
+    progress: 85,
+    skillType: 'Language',
+    nameInLogo: true,
+  },
+  {
+    iconName: 'D3',
+    iconURL: '../img/tech_icons/d3.svg',
+    progress: 60,
+    skillType: 'Front-End Framework / Library',
+    nameInLogo: true,
+  },
+  {
+    iconName: 'PHP',
+    iconURL: '../img/tech_icons/php.svg',
+    progress: 60,
+    skillType: 'Language',
+    nameInLogo: true,
+  },
+  {
+    iconName: 'MySQL',
+    iconURL: '../img/tech_icons/mysql.svg',
+    progress: 45,
+    skillType: 'Database',
+    nameInLogo: true,
+  },
+  {
+    iconName: 'MongoDB',
+    iconURL: '../img/tech_icons/mongodb.svg',
+    progress: 30,
+    skillType: 'Database',
+    nameInLogo: true,
+  },
+  {
+    iconName: 'GraphQL',
+    iconURL: '../img/tech_icons/graphql.svg',
+    progress: 30,
+    skillType: 'API',
+    nameInLogo: false,
+  },
+  {
+    iconName: 'Python',
+    iconURL: '../img/tech_icons/python.svg',
+    progress: 60,
+    skillType: 'Language',
+    nameInLogo: false,
+  },
+  {
+    iconName: 'Git',
+    iconURL: '../img/tech_icons/git.svg',
+    progress: 75,
+    skillType: 'Tool',
+    nameInLogo: true,
+  },
+  {
+    iconName: 'Swift',
+    iconURL: '../img/tech_icons/swift.svg',
+    progress: 30,
+    skillType: 'Language',
+    nameInLogo: false,
+  },
+  {
+    iconName: 'SwiftUI',
+    iconURL: '../img/tech_icons/swiftui.svg',
+    progress: 30,
+    skillType: 'UI Framework',
+    nameInLogo: false,
+  },
+  {
+    iconName: 'jQuery',
+    iconURL: '../img/tech_icons/jquery.svg',
+    progress: 80,
+    skillType: 'Front-End Framework / Library',
+    nameInLogo: true,
+  },
 ];
 
 export const iconsWithClasses = [
-	'React & React Native', 'D3', 'MySQL', 'NodeJS', 'jQuery', 'Git', 'MongoDB', 'TailwindCSS', 'NextJS'
+  'React & React Native',
+  'D3',
+  'MySQL',
+  'NodeJS',
+  'jQuery',
+  'Git',
+  'MongoDB',
+  'TailwindCSS',
+  'NextJS',
 ];
