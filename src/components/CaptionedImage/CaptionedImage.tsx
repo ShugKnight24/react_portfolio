db6b02565647820a655fb33511bbac205b718a34
Shugmi Shumunov <SShumunov@gmail.com>
2024-09-05 11:07:56 -0400

    fix: Correct display of `CaptionedImage`

--- src/components/CaptionedImage/CaptionedImage.tsx
+++ src/components/CaptionedImage/CaptionedImage.tsx
@@ -3,9 +3,9 @@ import { FC } from 'react';
 import { CaptionedImageProps } from '../../types/captionedImage';
 
 export const CaptionedImage: FC<CaptionedImageProps> = ({ src, alt, caption, link, className }) => (
-  <div className={`image-container ${className}`}>
+  <div className="image-container">
     <figure>
-      <img src={src} alt={alt} />
+      <img className={className} src={src} alt={alt} />
       <figcaption>
         {link ? (
           <a href={link} rel="noopener noreferrer" target="_blank">
